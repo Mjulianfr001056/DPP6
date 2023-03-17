21ec403daffa3c74e378baf2cc27b794a69c9690
tar1405 <irestu549@gmail.com>
2023-03-17 16:25:14 +0700

    Test ?

---
+++ src/dpp6/testRestu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dpp6;
+
+/**
+ *
+ * @author user
+ */
+public class testRestu {
+    public static void main(String Args[]) {
+        System.out.println("Halo Restu");
+        System.out.println("Selamat Datang !!!");
+    }
+}
