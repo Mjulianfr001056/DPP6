fe887ce719662faa325bc907cca2accbc82dfb9f
nataliemerry <222112243@stis.ac.id>
2023-03-17 15:02:47 +0700

    tes meri

---
+++ src/dpp6/meri.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dpp6;
+
+/**
+ *
+ * @author ACER
+ */
+public class meri {
+    public static void main(String[] args) {
+        System.out.println("Halo bang ju");
+    }
+}
