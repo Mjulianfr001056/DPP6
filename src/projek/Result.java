f08e811f2b7f900f0c6cc9d65cd26af1068ee5ea
rifkymptra <rifky.mputra8@gmail.com>
2023-03-22 12:51:10 +0700

    Update rearrange

--- src/projek/Result.java
+++ src/projek/Result.java
@@ -12,68 +12,68 @@ public class Result {
     private boolean kehutanan=false;
     private boolean perikanan=false;
 
-    public void setKonfirmasi(boolean konfirmasi) {
-        this.konfirmasi = konfirmasi;
-    }
-
-    public void setKondisiPerusahaan(int kondisiPerusahaan) {
-        this.kondisiPerusahaan = kondisiPerusahaan;
-    }
-
-    public void setTanamanPangan(boolean tanamanPangan) {
-        this.tanamanPangan = tanamanPangan;
-    }
-
-    public void setHortikultura(boolean hortikultura) {
-        this.hortikultura = hortikultura;
-    }
-
-    public void setPerkebunan(boolean perkebunan) {
-        this.perkebunan = perkebunan;
-    }
-
-    public void setPeternakan(boolean peternakan) {
-        this.peternakan = peternakan;
-    }
-
-    public void setKehutanan(boolean kehutanan) {
-        this.kehutanan = kehutanan;
-    }
-
-    public void setPerikanan(boolean perikanan) {
-        this.perikanan = perikanan;
-    }
-
     public boolean isKonfirmasi() {
         return konfirmasi;
+    }
+
+    public void setKonfirmasi(boolean konfirmasi) {
+        this.konfirmasi = konfirmasi;
     }
 
     public int getKondisiPerusahaan() {
         return kondisiPerusahaan;
     }
 
+    public void setKondisiPerusahaan(int kondisiPerusahaan) {
+        this.kondisiPerusahaan = kondisiPerusahaan;
+    }
+
     public boolean isTanamanPangan() {
         return tanamanPangan;
+    }
+
+    public void setTanamanPangan(boolean tanamanPangan) {
+        this.tanamanPangan = tanamanPangan;
     }
 
     public boolean isHortikultura() {
         return hortikultura;
     }
 
+    public void setHortikultura(boolean hortikultura) {
+        this.hortikultura = hortikultura;
+    }
+
     public boolean isPerkebunan() {
         return perkebunan;
+    }
+
+    public void setPerkebunan(boolean perkebunan) {
+        this.perkebunan = perkebunan;
     }
 
     public boolean isPeternakan() {
         return peternakan;
     }
 
+    public void setPeternakan(boolean peternakan) {
+        this.peternakan = peternakan;
+    }
+
     public boolean isKehutanan() {
         return kehutanan;
+    }
+
+    public void setKehutanan(boolean kehutanan) {
+        this.kehutanan = kehutanan;
     }
 
     public boolean isPerikanan() {
         return perikanan;
     }
-    
+
+    public void setPerikanan(boolean perikanan) {
+        this.perikanan = perikanan;
+    }
+
 }
