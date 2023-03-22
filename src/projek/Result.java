03072601371a737f7a5940e862de575e3a4c70f1
rifkymptra <rifky.mputra8@gmail.com>
2023-03-22 12:28:03 +0700

    update class result dan revisi

--- src/projek/Result.java
+++ src/projek/Result.java
@@ -3,51 +3,77 @@ package projek;
 
 public class Result {
     
-    private boolean konfirmasi;
-    private int kondisiPerusahaan; 
-    private boolean tanamanPangan;
-    private boolean hortikultura;
-    private boolean perkebunan;
-    private boolean peternakan;
-    private boolean kehutanan;
-    private boolean perikanan;
-   
-    public void setKonfirmasi(boolean konfirmasi)
-    {
+    private boolean konfirmasi=false;
+    private int kondisiPerusahaan=0; 
+    private boolean tanamanPangan=false;
+    private boolean hortikultura=false;
+    private boolean perkebunan=false;
+    private boolean peternakan=false;
+    private boolean kehutanan=false;
+    private boolean perikanan=false;
+
+    public void setKonfirmasi(boolean konfirmasi) {
         this.konfirmasi = konfirmasi;
-    }
-    
-    public boolean getKonfirmasi()
-    {
-        return konfirmasi;
     }
 
     public void setKondisiPerusahaan(int kondisiPerusahaan) {
         this.kondisiPerusahaan = kondisiPerusahaan;
     }
 
-    public int getKondisiPerusahaan() {
-        return kondisiPerusahaan;
-    }
-    
-
     public void setTanamanPangan(boolean tanamanPangan) {
         this.tanamanPangan = tanamanPangan;
-    }
-    
-    public boolean getTanamanPangan() {
-        return tanamanPangan;
-        
     }
 
     public void setHortikultura(boolean hortikultura) {
         this.hortikultura = hortikultura;
     }
-    
-    public boolean getHortikultura()
-    {
+
+    public void setPerkebunan(boolean perkebunan) {
+        this.perkebunan = perkebunan;
+    }
+
+    public void setPeternakan(boolean peternakan) {
+        this.peternakan = peternakan;
+    }
+
+    public void setKehutanan(boolean kehutanan) {
+        this.kehutanan = kehutanan;
+    }
+
+    public void setPerikanan(boolean perikanan) {
+        this.perikanan = perikanan;
+    }
+
+    public boolean isKonfirmasi() {
+        return konfirmasi;
+    }
+
+    public int getKondisiPerusahaan() {
+        return kondisiPerusahaan;
+    }
+
+    public boolean isTanamanPangan() {
+        return tanamanPangan;
+    }
+
+    public boolean isHortikultura() {
         return hortikultura;
     }
-    
+
+    public boolean isPerkebunan() {
+        return perkebunan;
+    }
+
+    public boolean isPeternakan() {
+        return peternakan;
+    }
+
+    public boolean isKehutanan() {
+        return kehutanan;
+    }
+
+    public boolean isPerikanan() {
+        return perikanan;
+    }
     
 }
