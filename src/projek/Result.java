f858da67d856c93d94875cb68f17a8f06c466c95
RizkyRd <rizkyrahmadanierf@gmail.com>
2023-03-22 12:19:43 +0700

    update buat getset result class

--- src/projek/Result.java
+++ src/projek/Result.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package projek;
 
-/**
- *
- * @author HP
- */
 public class Result {
+    
+    private boolean konfirmasi;
+    private int kondisiPerusahaan; 
+    private boolean tanamanPangan;
+    private boolean hortikultura;
+    private boolean perkebunan;
+    private boolean peternakan;
+    private boolean kehutanan;
+    private boolean perikanan;
+   
+    public void setKonfirmasi(boolean konfirmasi)
+    {
+        this.konfirmasi = konfirmasi;
+    }
+    
+    public boolean getKonfirmasi()
+    {
+        return konfirmasi;
+    }
+
+    public void setKondisiPerusahaan(int kondisiPerusahaan) {
+        this.kondisiPerusahaan = kondisiPerusahaan;
+    }
+
+    public int getKondisiPerusahaan() {
+        return kondisiPerusahaan;
+    }
+    
+
+    public void setTanamanPangan(boolean tanamanPangan) {
+        this.tanamanPangan = tanamanPangan;
+    }
+    
+    public boolean getTanamanPangan() {
+        return tanamanPangan;
+        
+    }
+
+    public void setHortikultura(boolean hortikultura) {
+        this.hortikultura = hortikultura;
+    }
+    
+    public boolean getHortikultura()
+    {
+        return hortikultura;
+    }
+    
     
 }
