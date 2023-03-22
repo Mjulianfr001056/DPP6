7d7df9528db76a5ec602bef6807359a789106812
rifkymptra <rifky.mputra8@gmail.com>
2023-03-22 12:40:04 +0700

    Rearrange result class

--- src/projek/Result.java
+++ src/projek/Result.java
@@ -12,68 +12,6 @@ public class Result {
     private boolean kehutanan=false;
     private boolean perikanan=false;
 
-    public void setKonfirmasi(boolean konfirmasi) {
-        this.konfirmasi = konfirmasi;
-    }
-
-    public void setKondisiPerusahaan(int kondisiPerusahaan) {
-        this.kondisiPerusahaan = kondisiPerusahaan;
-    }
-
-    public void setTanamanPangan(boolean tanamanPangan) {
-        this.tanamanPangan = tanamanPangan;
-    }
-
-    public void setHortikultura(boolean hortikultura) {
-        this.hortikultura = hortikultura;
-    }
-
-    public void setPerkebunan(boolean perkebunan) {
-        this.perkebunan = perkebunan;
-    }
-
-    public void setPeternakan(boolean peternakan) {
-        this.peternakan = peternakan;
-    }
-
-    public void setKehutanan(boolean kehutanan) {
-        this.kehutanan = kehutanan;
-    }
-
-    public void setPerikanan(boolean perikanan) {
-        this.perikanan = perikanan;
-    }
-
-    public boolean isKonfirmasi() {
-        return konfirmasi;
-    }
-
-    public int getKondisiPerusahaan() {
-        return kondisiPerusahaan;
-    }
-
-    public boolean isTanamanPangan() {
-        return tanamanPangan;
-    }
-
-    public boolean isHortikultura() {
-        return hortikultura;
-    }
-
-    public boolean isPerkebunan() {
-        return perkebunan;
-    }
-
-    public boolean isPeternakan() {
-        return peternakan;
-    }
-
-    public boolean isKehutanan() {
-        return kehutanan;
-    }
-
-    public boolean isPerikanan() {
-        return perikanan;
-    }
+    
     
 }
