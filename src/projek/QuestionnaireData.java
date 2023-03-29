34008fda9a4225027253b6af705253426f2be98c
RizkyRd <rizkyrahmadanierf@gmail.com>
2023-03-30 02:14:37 +0700

    Update Terbaru rizky

--- src/projek/QuestionnaireData.java
+++ src/projek/QuestionnaireData.java
@@ -4,85 +4,141 @@ package projek;
 import java.util.HashMap;
 
 public class QuestionnaireData {
+   
+    private QuestionnaireValidator<Boolean> confirmed;           // kolom 12
+    private QuestionnaireValidator<Integer> kondisiPerusahaan;   // kolom 13
+    private QuestionnaireValidator<Boolean> tanamanPangan;       // kolom 14
+    private QuestionnaireValidator<Boolean> hortikultura;        // kolom 15
+    private QuestionnaireValidator<String>  perkebunan;           // kolom 16
+    private QuestionnaireValidator<Boolean> peternakan;          // kolom 17
+    private QuestionnaireValidator<Boolean> kehutanan;           // kolom 18
+    private QuestionnaireValidator<Boolean> perikanan;           // kolom 19
+    public HashMap<Integer,String> ketint = new HashMap<>();
+    public HashMap<String,String>  ketstr = new HashMap<>();
     
-//    private KeteranganKuesioner keterangan;
-    private QuestionnaireValidator<Boolean> confirmed;       // kolom 12
-    private KeteranganKuesioner kondisiPerusahaan;                        // kolom 13
-    private QuestionnaireValidator<Boolean> tanamanPangan;   // kolom 14
-    private QuestionnaireValidator<Boolean> hortikultura;    // kolom 15
-    private KeteranganKuesioner perkebunan;                               // kolom 16
-    private QuestionnaireValidator<Boolean> peternakan;      // kolom 17
-    private QuestionnaireValidator<Boolean> kehutanan;       // kolom 18
-    private QuestionnaireValidator<Boolean> perikanan;       // kolom 19
-    private Boolean status;
+    public QuestionnaireData()
+    {
+        //Kondisi Perusahaan
+        ketint.put(1, "Aktif");
+        ketint.put(2, "Tutup Sementara/Tidak Ada Kegiatan");
+        ketint.put(3, "Belum Berproduksi");
+        ketint.put(4, "Tidak Bersedia Diwawancarai");
+        ketint.put(5, "Alih Usaha ke Non Pertanian");
+        ketint.put(6, "Tutup");
+        ketint.put(7, "Tidak Ditemukan");
+        ketint.put(8, "Baru");
+        ketint.put(9, "Ganda");
+        
+        //Subsektor Perkebunan
+        ketstr.put("3a","Kakao/Cokelat");
+        ketstr.put("3b", "Karet");
+        ketstr.put("3c", "Kelapa Sawit");
+        ketstr.put("3d", "Kopi");
+        ketstr.put("3e", "Teh");
+        ketstr.put("3f", "Tebu");
+        ketstr.put("3g", "Tembakau");
+        ketstr.put("3h", "Cengkih");
+        ketstr.put("3i", "Kelapa");
+        ketstr.put("3j", "Lada");
+        ketstr.put("3k", "Tanaman Lainnya");
+        ketstr.put("0", "Tidak Ada");
+    }
     
-    public QuestionnaireValidator<Boolean> getConfirmed() {
-        return confirmed;
+    
+    public boolean getConfirmed() {
+        return confirmed.getAttribute().booleanValue();
     }
 
     public void setConfirmed(String confirmed) throws QuestionnaireException{
+        try {
         this.confirmed = new BooleanField(confirmed);
+        } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
-    public String getKondisiPerusahaan(String kondisiPerusahaan1) {    
-       return kondisiPerusahaan.getKeteranganKuesioner(kondisiPerusahaan1);
+    public int getKondisiPerusahaan() {    
+       return kondisiPerusahaan.getAttribute().intValue();
     }
 
-//    public void setKondisiPerusahaan(String kondisiPerusahaan1) {
-//        this.kondisiPerusahaan.getKeteranganKuesioner(kondisiPerusahaan1) ;
-//    }
+    public void setKondisiPerusahaan(String kondisiPerusahaan1) throws QuestionnaireException {
+       try {
+        this.kondisiPerusahaan = new IntegerField(kondisiPerusahaan1);
+       } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+    }
 
-    public QuestionnaireValidator<Boolean> getTanamanPangan() {
-        return tanamanPangan;
+    public boolean getTanamanPangan() {
+        return tanamanPangan.getAttribute().booleanValue();
     }
 
     public void setTanamanPangan(String tanamanPangan1) throws QuestionnaireException {
+        try {
         this.tanamanPangan = new BooleanField(tanamanPangan1);
+        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
-    public QuestionnaireValidator<Boolean> getHortikultura() {
-        return hortikultura;
+    public boolean getHortikultura() {
+        return hortikultura.getAttribute().booleanValue();
     }
 
     public void setHortikultura(String hortikultura1) throws QuestionnaireException {
-          this.hortikultura = new BooleanField(hortikultura1);
+        try {  
+        this.hortikultura = new BooleanField(hortikultura1);
+        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
-    public String getPerkebunan(String perkebunan1) {
-        return perkebunan.getKeteranganKuesioner(perkebunan1);
+    public String getPerkebunan() {
+        return perkebunan.getAttribute().toString();
     }
 
-//    public void setPerkebunan(String perkebunan1) {
-//        this.perkebunan = new BooleanField(perkebunan1);
-//    }
+    public void setPerkebunan(String perkebunan1) throws QuestionnaireException {
+        try {
+        this.perkebunan = new StringField(perkebunan1);
+        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+    }
 
-    public QuestionnaireValidator<Boolean> getPeternakan() {
-        return peternakan;
+    public boolean getPeternakan() {
+        return peternakan.getAttribute().booleanValue();
     }
 
     public void setPeternakan(String peternakan1) throws QuestionnaireException {
+       try {
         this.peternakan = new BooleanField(peternakan1);
+       } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
-    public QuestionnaireValidator<Boolean> getKehutanan() {
-        return kehutanan;
+    public boolean getKehutanan() {
+        return kehutanan.getAttribute().booleanValue();
     }
 
     public void setKehutanan(String kehutanan1) throws QuestionnaireException {
+       try{
         this.kehutanan = new BooleanField(kehutanan1);
+       } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
-    public QuestionnaireValidator<Boolean> getPerikanan() {
-        return perikanan;
+    public boolean getPerikanan() {
+        return perikanan.getAttribute().booleanValue();
     }
 
     public void setPerikanan(String perikanan1) throws QuestionnaireException {
+       try{
         this.perikanan = new BooleanField(perikanan1);
+       } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
+    
+    @Override
+    public String toString()
+    {
+        return "Informasi Kunjungan : "+getConfirmed()+
+                "\nKondisi Perusahaan : "+getKondisiPerusahaan()+
+                "\nTanaman Pangan : "+getTanamanPangan()+
+                "\nHortikultura : "+getHortikultura()+
+                "\nPerkebunan : "+getPerkebunan()+
+                "\nPeternakan : "+getPeternakan()+
+                "\nKehutanan : "+getKehutanan()+
+                "\nPerikanan : "+getPerikanan();
+    }
 }
 
-/*
-TODO sesuaikan lagi get dan set nya
- */
+
 
--- src/projek/QuestionnaireValidator.java
+++ src/projek/QuestionnaireValidator.java
@@ -2,7 +2,7 @@ package projek;
 
 public interface QuestionnaireValidator<R> {
     R getAttribute();
-    boolean isValid(String attributes) throws QuestionnaireException;  
+     
 }
 
 class BooleanField implements QuestionnaireValidator<Boolean> {
@@ -10,21 +10,15 @@ class BooleanField implements QuestionnaireValidator<Boolean> {
 
     public BooleanField(String attribute) throws QuestionnaireException{
         this.attribute = isValid(attribute);
-    }
+   }
 
     @Override
     public Boolean getAttribute() {
         return attribute;
     }
 
-    @Override
     public boolean isValid(String attributes) throws QuestionnaireException {
-        // implementation
-        /*
-        TODO confirmation
-        Buat validasi klo yang dimasukkan itu 1 atau 0
-         */
-       
+        
         if (attributes == "1")
            return true;
          else if (attributes == "0")
@@ -35,7 +29,85 @@ class BooleanField implements QuestionnaireValidator<Boolean> {
     }  
 }
 
-/*
-TODO buat IntegerField class
-Buat field untuk integer
- */
+
+//Integer Field
+
+class IntegerField implements QuestionnaireValidator<Integer> {
+    private Integer attribute;
+    private QuestionnaireData i; 
+    
+    public IntegerField(String attribute) throws QuestionnaireException{
+             this.attribute = isValid(attribute);
+    }
+
+    @Override
+    public Integer getAttribute() {
+        return attribute; 
+    }
+    
+    @Override
+    public String toString() //method get baru khusus untuk mengembalikan keterangan kondisi perusahaan
+    { 
+        String ket = i.ketint.get(String.valueOf(attribute));
+        return ket;
+    }
+    
+    public Integer isValid(String attributes) throws QuestionnaireException {
+          
+       if (!(attributes.matches("\\d") || attributes.matches("[123456789]")))
+       {
+            throw new QuestionnaireException("Inputan Anda salah");
+       }
+         else 
+             return Integer.getInteger(attributes);
+            
+              
+    }    
+}
+
+
+
+// String Field
+
+class StringField implements QuestionnaireValidator<String> {
+    private String attribute;
+    private QuestionnaireData i;
+
+    public StringField(String attribute) throws QuestionnaireException{
+       this.attribute = isValid(attribute);         
+   }
+
+    @Override
+    public String getAttribute() {
+        return attribute;
+    }
+    
+    @Override
+    public String toString() //method get baru khusus untuk mengembalikan keterangan kode Perkebunan
+    {
+        String ket = i.ketstr.get(attribute);
+        return ket;
+    }
+
+    
+    public String isValid(String attributes) throws QuestionnaireException {
+        
+        if (!(attributes == "3a" ||
+            attributes == "3b" ||
+            attributes == "3c" ||
+            attributes == "3d" ||
+            attributes == "3e" ||
+            attributes == "3f" ||
+            attributes == "3g" ||
+            attributes == "3h" ||
+            attributes == "3i" ||
+            attributes == "3j" ||
+            attributes == "3k" ||
+            attributes == "0"))
+           throw new QuestionnaireException("Input anda Salah");
+        
+         else return attributes;
+        
+              
+    }  
+}
