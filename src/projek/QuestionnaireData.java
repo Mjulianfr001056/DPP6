b07a270e8f6dae2fc49733f261bd6b4389a220a2
Mjulianfr001056 <222112217@stis.ac.id>
2023-03-30 11:22:54 +0700

    Revised QuestionnaireData and Validator

--- src/projek/QuestionnaireData.java
+++ src/projek/QuestionnaireData.java
@@ -13,100 +13,75 @@ public class QuestionnaireData {
     private QuestionnaireValidator<Boolean> peternakan;          // kolom 17
     private QuestionnaireValidator<Boolean> kehutanan;           // kolom 18
     private QuestionnaireValidator<Boolean> perikanan;           // kolom 19
-    public HashMap<Integer,String> ketint = new HashMap<>();
-    public HashMap<String,String>  ketstr = new HashMap<>();
-    public HashMap<Boolean,String> ketbool = new HashMap<>();
-    
-    public QuestionnaireData()
-    {
-        //Kondisi Perusahaan
-        ketint.put(1, "Aktif");
-        ketint.put(2, "Tutup Sementara/Tidak Ada Kegiatan");
-        ketint.put(3, "Belum Berproduksi");
-        ketint.put(4, "Tidak Bersedia Diwawancarai");
-        ketint.put(5, "Alih Usaha ke Non Pertanian");
-        ketint.put(6, "Tutup");
-        ketint.put(7, "Tidak Ditemukan");
-        ketint.put(8, "Baru");
-        ketint.put(9, "Ganda");
-        
-        //Subsektor Perkebunan
-        ketstr.put("3a","Kakao/Cokelat");
-        ketstr.put("3b", "Karet");
-        ketstr.put("3c", "Kelapa Sawit");
-        ketstr.put("3d", "Kopi");
-        ketstr.put("3e", "Teh");
-        ketstr.put("3f", "Tebu");
-        ketstr.put("3g", "Tembakau");
-        ketstr.put("3h", "Cengkih");
-        ketstr.put("3i", "Kelapa");
-        ketstr.put("3j", "Lada");
-        ketstr.put("3k", "Tanaman Lainnya");
-        ketstr.put("0", "Tidak Ada");
-        
-        //Field Boolean
-        ketbool.put(true, "Ada");
-        ketbool.put(false, "Tidak Ada");
-    }
-    
     
     public boolean getConfirmed() {
         return confirmed.getAttribute().booleanValue();
     }
 
-    public void setConfirmed(String confirmed) throws QuestionnaireException{
+    public void setConfirmed(String confirmed){
         try {
-        this.confirmed = new BooleanField(confirmed);
-        } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
+            this.confirmed = new BooleanField(confirmed);
+        } catch (QuestionnaireException e) {
+            System.out.println("Isian Salah");
+        }
     }
 
     public int getKondisiPerusahaan() {    
        return kondisiPerusahaan.getAttribute().intValue();
     }
 
-    public void setKondisiPerusahaan(String kondisiPerusahaan1) throws QuestionnaireException {
+    public void setKondisiPerusahaan(String kondisiPerusahaan){
        try {
-        this.kondisiPerusahaan = new IntegerField(kondisiPerusahaan1);
-       } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+           this.kondisiPerusahaan = new IntegerField(kondisiPerusahaan);
+       } catch(QuestionnaireException e) {
+           System.out.println("Isian Salah");
+       }
     }
 
     public boolean getTanamanPangan() {
         return tanamanPangan.getAttribute().booleanValue();
     }
 
-    public void setTanamanPangan(String tanamanPangan1) throws QuestionnaireException {
+    public void setTanamanPangan(String tanamanPangan){
         try {
-        this.tanamanPangan = new BooleanField(tanamanPangan1);
-        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+            this.tanamanPangan = new BooleanField(tanamanPangan);
+        } catch(QuestionnaireException e) {
+            System.out.println("Isian Salah");
+        }
     }
 
     public boolean getHortikultura() {
         return hortikultura.getAttribute().booleanValue();
     }
 
-    public void setHortikultura(String hortikultura1) throws QuestionnaireException {
-        try {  
-        this.hortikultura = new BooleanField(hortikultura1);
-        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+    public void setHortikultura(String hortikultura){
+        try {
+            this.hortikultura = new BooleanField(hortikultura);
+        } catch(QuestionnaireException e) {
+            System.out.println("Isian Salah");
+        }
     }
 
     public String getPerkebunan() {
-        return perkebunan.getAttribute().toString();
+        //return perkebunan.getAttribute().toString();
+        return perkebunan.getAttribute();
     }
 
-    public void setPerkebunan(String perkebunan1) throws QuestionnaireException {
+    public void setPerkebunan(String perkebunan){
         try {
-        this.perkebunan = new StringField(perkebunan1);
-        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
+            this.perkebunan = new StringField(perkebunan);
+        } catch(QuestionnaireException e) {
+            System.out.println("Isian Salah");
+        }
     }
 
     public boolean getPeternakan() {
         return peternakan.getAttribute().booleanValue();
     }
 
-    public void setPeternakan(String peternakan1) throws QuestionnaireException {
+    public void setPeternakan(String peternakan){
        try {
-        this.peternakan = new BooleanField(peternakan1);
+        this.peternakan = new BooleanField(peternakan);
        } catch(QuestionnaireException e) {System.out.println("Isian Salah");}
     }
 
@@ -114,36 +89,103 @@ public class QuestionnaireData {
         return kehutanan.getAttribute().booleanValue();
     }
 
-    public void setKehutanan(String kehutanan1) throws QuestionnaireException {
+    public void setKehutanan(String kehutanan){
        try{
-        this.kehutanan = new BooleanField(kehutanan1);
-       } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
+           this.kehutanan = new BooleanField(kehutanan);
+       } catch (QuestionnaireException e) {
+           System.out.println("Isian Salah");
+       }
     }
 
     public boolean getPerikanan() {
         return perikanan.getAttribute().booleanValue();
     }
 
-    public void setPerikanan(String perikanan1) throws QuestionnaireException {
+    public void setPerikanan(String perikanan){
        try{
-        this.perikanan = new BooleanField(perikanan1);
-       } catch (QuestionnaireException e) {System.out.println("Isian Salah");}
+           this.perikanan = new BooleanField(perikanan);
+       } catch (QuestionnaireException e) {
+           System.out.println("Isian Salah");
+       }
     }
 
+    public static String map(Boolean mapped){
+        return mapped ? "Ya" : "Tidak";
+    }
+
+    public static String map(Integer mapped){
+        return KondisiPerusahaanMap.getInstance().getValue(mapped.intValue());
+    }
+
+    public static String map(String mapped){
+        return PerkebunanMap.getInstance().getValue(mapped);
+    }
     
     @Override
     public String toString()
     {
-        return "Informasi Kunjungan : "+getConfirmed()+
-                "\nKondisi Perusahaan : "+getKondisiPerusahaan()+
-                "\nTanaman Pangan : "+getTanamanPangan()+
-                "\nHortikultura : "+getHortikultura()+
-                "\nPerkebunan : "+getPerkebunan()+
-                "\nPeternakan : "+getPeternakan()+
-                "\nKehutanan : "+getKehutanan()+
-                "\nPerikanan : "+getPerikanan();
+        return "Informasi Kunjungan : "+ map(getConfirmed())+
+                "\nKondisi Perusahaan : "+map(getKondisiPerusahaan())+
+                "\nTanaman Pangan : "+map(getTanamanPangan())+
+                "\nHortikultura : "+map(getHortikultura())+
+                "\nPerkebunan : "+map(getPerkebunan())+
+                "\nPeternakan : "+map(getPeternakan())+
+                "\nKehutanan : "+map(getKehutanan())+
+                "\nPerikanan : "+map(getPerikanan());
     }
 }
 
+final class KondisiPerusahaanMap {
+    private static final KondisiPerusahaanMap INSTANCE = new KondisiPerusahaanMap();
+    private final HashMap<Integer, String> map = new HashMap<>();
+
+    private KondisiPerusahaanMap() {
+        map.put(1, "Aktif");
+        map.put(2, "Tutup Sementara/Tidak Ada Kegiatan");
+        map.put(3, "Belum Berproduksi");
+        map.put(4, "Tidak Bersedia Diwawancarai");
+        map.put(5, "Alih Usaha ke Non Pertanian");
+        map.put(6, "Tutup");
+        map.put(7, "Tidak Ditemukan");
+        map.put(8, "Baru");
+        map.put(9, "Ganda");
+    }
+
+    public static KondisiPerusahaanMap getInstance() {
+        return INSTANCE;
+    }
+
+    public String getValue(int key) {
+        return map.get(key);
+    }
+}
+
+final class PerkebunanMap {
+    private static final PerkebunanMap INSTANCE = new PerkebunanMap();
+    private final HashMap<String, String> map = new HashMap<>();
+
+    private PerkebunanMap() {
+        map.put("3a","Kakao/Cokelat");
+        map.put("3b", "Karet");
+        map.put("3c", "Kelapa Sawit");
+        map.put("3d", "Kopi");
+        map.put("3e", "Teh");
+        map.put("3f", "Tebu");
+        map.put("3g", "Tembakau");
+        map.put("3h", "Cengkih");
+        map.put("3i", "Kelapa");
+        map.put("3j", "Lada");
+        map.put("3k", "Tanaman Lainnya");
+        map.put("0", "Tidak Ada");
+    }
+
+    public static PerkebunanMap getInstance() {
+        return INSTANCE;
+    }
+
+    public String getValue(String key) {
+        return map.get(key);
+    }
+}
 
 
