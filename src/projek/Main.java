09f599123598728119c5565786bf48ebd7893fb5
Mjulianfr001056 <222112217@stis.ac.id>
2023-04-08 13:32:42 +0700

    Add colored output

--- src/projek/Main.java
+++ src/projek/Main.java
@@ -30,19 +30,19 @@ public class Main {
 
     private static void tampilHeader(){
         if (headerFlag){
-            System.out.println("Kode Provinsi: " + questionnaireHeader.kodeProvinsi());
-            System.out.println("Kode Kabupaten: " + questionnaireHeader.kodeKabupaten());
-            System.out.println("Periode Kuesioner: " + questionnaireHeader.tanggal());
+            System.out.println("Kode Provinsi: \u001B[94m" + questionnaireHeader.kodeProvinsi());
+            System.out.println("\u001B[0mKode Kabupaten: \u001B[94m" + questionnaireHeader.kodeKabupaten());
+            System.out.println("\u001B[0mPeriode Kuesioner: \u001B[94m" + questionnaireHeader.tanggal() + "\u001B[0m");
         }else{
-            System.out.println("Kode Provinsi: [Belum disetel]");
-            System.out.println("Kode Kabupaten: [Belum disetel]");
-            System.out.println("Periode Kuesioner: [Belum disetel]");
+            System.out.println("Kode Provinsi: \033[31m[Belum disetel]\033[0m");
+            System.out.println("Kode Kabupaten: \033[31m[Belum disetel]\033[0m");
+            System.out.println("Periode Kuesioner: \033[31m[Belum disetel]\033[0m");
         }
     }
 
     public static void printData(){
         if (sheets.size() == 0)
-            System.out.println("Tidak ada data yang dapat ditampilkan, harap lakukan input data terlebih dahulu!\n");
+            System.out.println("\033[31mTidak ada data yang dapat ditampilkan, harap lakukan input data terlebih dahulu!\033[0m\n");
         else
             sheets.stream().forEach(data-> System.out.println(data + "\n"));
     }
@@ -52,17 +52,18 @@ public class Main {
         Scanner in = new Scanner(System.in);
         boolean quit = false;
         do {
-            System.out.println("\n####################################################################################\n");
+            System.out.println("\u001B[34m\n####################################################################################\n\u001B[0m");
             tampilHeader();
-            System.out.println("\n------------------------------------------------------------------------------------\n");
+            System.out.println("\u001B[34m\n------------------------------------------------------------------------------------\n\u001B[0m");
             System.out.println("Silakan Pilih Menu yang Tersedia!");
-            System.out.println("Pilihan Menu: ");
-            System.out.println("1. Entri data");
-            System.out.println("2. Lihat data");
-            System.out.println("3. Keluar");
+            System.out.println("Menu: ");
+            System.out.println("\033[33m1. Entri data");
+            System.out.println("\033[33m2. Lihat data");
+            System.out.println("\033[33m3. Keluar");
+            System.out.print("\033[0m");
             System.out.println("-------------");
         
-            System.out.print("Pilihan Menu: ");
+            System.out.print("Pilih: ");
             String menu = in.next();
             ClearInputBuffer(in);
 
@@ -70,7 +71,8 @@ public class Main {
                 case "1" -> {
                     if(!headerFlag)
                     try {
-                        System.out.println("\n==================Bagian Header Kuesioner==================");
+                        System.out.println("\n\033[34m==================Bagian Header Kuesioner==================\033[0m");
+                        System.out.print("\033[0m");
                         String tmpKodeProvinsi = "";
                         String tmpKodeKabupaten = "";
                         String periodeKuesioner = "";
@@ -94,7 +96,7 @@ public class Main {
                                 periodeKuesioner);
                         headerFlag = true;
                     }catch (Exception e) {
-                        System.out.println(e.getMessage());
+                        System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
                         break;
                     }
 
@@ -102,7 +104,8 @@ public class Main {
                     boolean quitFlag = false;
                     do {
                         try{
-                            System.out.println("\n===============Bagian Informasi Perusahaan 1================");
+                            System.out.println("\n\033[34m===============Bagian Informasi Perusahaan 1================\033[0m");
+                            System.out.print("\033[0m");
                             String tmpKodeProvinsi = "";
                             String tmpKodeKabupaten = "";
                             String tmpKodeKecamatan = "";
@@ -134,7 +137,7 @@ public class Main {
                             kip = new KIP(tmpKodeProvinsi, tmpKodeKabupaten, tmpKodeKecamatan, tmpKodeKJU, tmpNoUrut);
                             quitFlag = true;
                         }catch (Exception e) {
-                            System.out.println(e.getMessage());
+                            System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
                         }
                     }while(!quitFlag);
 
@@ -147,7 +150,8 @@ public class Main {
                             String tmpNomorFaksimili = "";
                             String tmpBentukBadanHukum = "";
 
-                            System.out.println("\n===============Bagian Informasi Perusahaan 2================");
+                            System.out.println("\n\033[34m===============Bagian Informasi Perusahaan 2================\033[0m");
+                            System.out.print("\033[0m");
                             System.out.print("Nama Perusahaan Pertanian: ");
                             tmpNamaPerusahaan = in.nextLine();
 
@@ -172,7 +176,8 @@ public class Main {
                             company.setBentukBadanHukum(tmpBentukBadanHukum);
                             quitFlag = true;
                         }catch (Exception e) {
-                            System.out.println(e.getMessage());
+                            System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
+                            quitFlag = false;
                         }
                     }while(!quitFlag);
 
@@ -181,9 +186,10 @@ public class Main {
                     quitFlag = false;
                     do {
                         try{
-                            System.out.println("\n================Bagian Respons Perusahaan================");
-                            System.out.println("1. Sudah dikunjungi/mendapatkan informasi dari pihak lain (dinas, dll)\n"
-                                    + "0. Belum dikonfirmasi keberadaannya");
+                            System.out.println("\n\033[34m================Bagian Respons Perusahaan================\033[0m");
+                            System.out.print("\033[0m");
+                            System.out.println("\u001B[33m1. Sudah dikunjungi/mendapatkan informasi dari pihak lain (dinas, dll)\n"
+                                    + "0. Belum dikonfirmasi keberadaannya\u001B[0m");
                             System.out.print("Pilih Keterangan Terkait Perusahaan: ");
                             String tmpConfirmed = in.next();
 
@@ -203,8 +209,9 @@ public class Main {
                             String tmpPerikanan = "";
                             String tmpJenisUsahaUtama = "";
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nStatus perusahaan: ");
-                            System.out.println("1. Aktif\n"
+                            System.out.println("\u001B[33m1. Aktif\n"
                                     + "2. Tutup Sementara/Tidak Ada Kegiatan\n"
                                     + "3. Belum Berproduksi\n"
                                     + "4. Tidak Bersedia Diwawancarai\n"
@@ -212,26 +219,29 @@ public class Main {
                                     + "6. Tutup\n"
                                     + "7. Tidak Ditemukan\n"
                                     + "8. Baru\n"
-                                    + "9. Ganda");
+                                    + "9. Ganda\u001B[0m");
                             System.out.print("Pilih Keterangan Terkait Perusahaan: ");
                             tmpKondisiPerusahaan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor tanaman pangan? ");
-                            System.out.println("0. Tidak\n"
-                                    + "1. Ya");
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "1. Ya\u001B[0m");
                             System.out.print("Jawab: ");
                             tmpTanamanPangan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor holtikultura? ");
-                            System.out.println("0. Tidak\n"
-                                    + "1. Ya");
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "1. Ya\u001B[0m");
                             System.out.print("Jawab: ");
                             tmpHortikultura = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor perkebunan? ");
-                            System.out.println("0. Tidak\n"
-                                    + "\nJika ya, spesialisasi subsektor adalah?\n"
-                                    + "3a. Kakao/Cokelat\n"
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "\u001B[0m\nJika ya, spesialisasi subsektor adalah?\n"
+                                    + "\u001B[33m3a. Kakao/Cokelat\n"
                                     + "3b. Karet\n"
                                     + "3c. Kelapa Sawit\n"
                                     + "3d. Kopi\n"
@@ -241,49 +251,53 @@ public class Main {
                                     + "3h. Cengkeh\n"
                                     + "3i. Kelapa\n"
                                     + "3j. Lada\n"
-                                    + "3k. Tanaman Lainnya");
+                                    + "3k. Tanaman Lainnya\u001B[0m");
                             System.out.print("Jawab: ");
                             tmpPerkebunan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor peternakan? ");
-                            System.out.println("0. Tidak\n"
-                                    + "\nJika ya, spesialisasi subsektor adalah?\n"
-                                    + "1. Ternak Sapi Perah\n"
-                                    + "2. Ternak Besar/Kecil\n"
-                                    + "3. Unggas");
-                            System.out.print("Jawab: ");
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "\u001B[0m\nJika ya, spesialisasi subsektor adalah?\n"
+                                    + "\033[33m1. Ternak Sapi Perah\n"
+                                    + "\033[33m2. Ternak Besar/Kecil\n"
+                                    + "\033[33m3. Unggas");
+                            System.out.print("\033[0mJawab: ");
                             tmpPeternakan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor kehutanan? ");
-                            System.out.println("0. Tidak\n"
-                                    + "\nJika ya, spesialisasi subsektor adalah?\n"
-                                    + "1. HPH/IUPHHK-HA\n"
-                                    + "2. HPHT/IUPHHK-HT/Perhutani/Kehutanan lainnya\n"
-                                    + "3. Penangkaran STL");
-                            System.out.print("Jawab: ");
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "\u001B[0m\nJika ya, spesialisasi subsektor adalah?\n"
+                                    + "\033[33m1. HPH/IUPHHK-HA\n"
+                                    + "\033[33m2. HPHT/IUPHHK-HT/Perhutani/Kehutanan lainnya\n"
+                                    + "\033[33m3. Penangkaran STL");
+                            System.out.print("\033[0mJawab: ");
                             tmpKehutanan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nPerusahaan dalam subsektor perikanan? ");
-                            System.out.println("0. Tidak Ada\n"
-                                    + "\nJika ya, spesialisasi subsektor adalah?\n"
-                                    + "1. Budidaya Udang\n"
-                                    + "2. Budidaya Bandeng\n"
-                                    + "3. Budidaya Ikan Lainnya di Tambak\n"
-                                    + "4. Budidaya Ikan di Laut\n"
-                                    + "5. Budidaya Ikan di Air Tawar\n"
-                                    + "6. Budidaya Pembenihan\n"
-                                    + "7. Penangkapan Ikan");
-                            System.out.print("Jawab: ");
+                            System.out.println("\u001B[33m0. Tidak\n"
+                                    + "\u001B[0m\nJika ya, spesialisasi subsektor adalah?\n"
+                                    + "\033[33m1. Budidaya Udang\n"
+                                    + "\033[33m2. Budidaya Bandeng\n"
+                                    + "\033[33m3. Budidaya Ikan Lainnya di Tambak\n"
+                                    + "\033[33m4. Budidaya Ikan di Laut\n"
+                                    + "\033[33m5. Budidaya Ikan di Air Tawar\n"
+                                    + "\033[33m6. Budidaya Pembenihan\n"
+                                    + "\033[33m7. Penangkapan Ikan");
+                            System.out.print("\033[0mJawab: ");
                             tmpPerikanan = in.next();
 
+                            System.out.println("\u001B[94m================================\u001B[0m");
                             System.out.println("\nJenis Usaha Utama: ");
-                            System.out.println("1. Tanaman Pangan\n"
-                                    + "2. Holtikultura\n"
-                                    + "3. Perkebunan\n"
-                                    + "4. Kehutanan\n"
-                                    + "5. Perikanan\n"
-                                    + "6. Peternakan");
-                            System.out.print("Pilih keterangan jenis usaha utama perusahaan: ");
+                            System.out.println("\033[33m1. Tanaman Pangan\n"
+                                    + "\033[33m2. Holtikultura\n"
+                                    + "\033[33m3. Perkebunan\n"
+                                    + "\033[33m4. Kehutanan\n"
+                                    + "\033[33m5. Perikanan\n"
+                                    + "\033[33m6. Peternakan");
+                            System.out.print("\033[0mPilih keterangan jenis usaha utama perusahaan: ");
                             tmpJenisUsahaUtama = in.next();
                             ClearInputBuffer(in);
 
@@ -300,40 +314,41 @@ public class Main {
                             companyResponse.setJenisUsahaUtama(tmpJenisUsahaUtama);
                             quitFlag = true;
                         }catch(Exception e) {
-                            System.out.println("Terjadi kesalahan input!");
-                            System.out.println(e.getMessage());
+                            System.out.println("\u001B[31m" + e.getMessage() + "\u001B[0m");
+                            quitFlag = false;
                         }
                     }while(!quitFlag);
-                    System.out.println("\nInput selesai");
+                    System.out.println("\u001B[32m\nInput selesai\u001B[0m");
                     sheets.add(new CompanyData(kip, company, companyResponse));
                 }
 
                 case "2" -> {
-                    System.out.println("Menampikan data...\n");
+                    System.out.println("\u001B[32mMenampikan data...\n\u001B[0m");
                     printData();
                 }
 
                 case "3" -> {
-                    System.out.println("Anda keluar, program diberhentikan.");
+                    System.out.println("\u001B[31mAnda keluar, program diberhentikan.\u001B[0m");
                     quit = true;
                 }
 
                 default -> {
-                    System.out.println("Input Tidak Terdaftar, Program Dihentikan");
+                    System.out.println("\u001B[31mInput Tidak Terdaftar, Program Dihentikan\u001B[0m");
                     quit = true;
                 }
-           }
+            }
        }
        while(!quit);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("=======================================");
+        System.out.println("\033[34m=======================================");
         System.out.println("== Program Entri Data Kuesioner DPP ==");
-        System.out.println("======================================\n");
+        System.out.println("=======================================");
+        System.out.println("\033[0m");
         System.out.println("Selamat Datang di Program Entri Data Kuesioner DPP (Direktori Perusahaan Pertanian)!\n");
-        System.out.println("Responden diharapkan dapat mengisi kuesioner ini dengan jujur dan benar.");
+        System.out.println("Isilah kuesioner ini dengan jujur dan benar!");
         System.out.println("Selamat mengisi kuesioner!\n");
         tampilMenu();        
     }
