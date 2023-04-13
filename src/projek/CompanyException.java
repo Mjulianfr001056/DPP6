652e09640d25265e245dfa4e798d76f767c31b6b
sabillahs <222112344@stis.ac.id>
2023-04-13 13:05:53 +0700

    finished

--- src/projek/CompanyException.java
+++ src/projek/CompanyException.java
@@ -1,5 +1,8 @@
 package projek;
 
+/**
+ * Merupakan class exception yang dapat mendeteksi dan menampilkan pesan terkait exception
+ */
 public class CompanyException extends Exception{
     public CompanyException(String message) {
         super(message);
