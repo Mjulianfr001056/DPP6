0e2c4d37b044638376bb1a05c7189af5c2b881fc
nataliemerry <222112243@stis.ac.id>
2023-04-14 01:07:02 +0700

    Merry-3

--- src/projek/NomorFaksimile.java
+++ src/projek/NomorFaksimile.java
@@ -4,9 +4,10 @@
  */
 package projek;
 
-/**
- *
- * @author ACER
+/** 
+ * Merupakan sebuah class yang berfungsi untuk mrnyimpan dan mencetak nomor faksimile
+ * class ini meng-extends abstract class NomorKontak
+ * @author Kelompok 6 DPP : Natalie Merry Angelina
  */
 public class NomorFaksimile extends NomorKontak{
     public NomorFaksimile(String nomorKontak){
@@ -18,6 +19,11 @@ public class NomorFaksimile extends NomorKontak{
 
     }
 
+    /**
+     * Merupakan method untuk mencetak nomor faksimile
+     * format nomor faksimile yang dicetak adalah (3 digit) digit yang tersisa
+     * contoh (021)777643
+     */
     @Override
     public void cetakNomor() {
         String nomorKontak = super.getNomorKontak();
